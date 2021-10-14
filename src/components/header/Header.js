6d4193d688163341d47a5120e3b26e1dbd6bbb83
Jesna k elizabeth jolly <92103227+Jesna-Dev@users.noreply.github.com>
2021-10-14 15:37:23 -0700

    newupdate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -49,6 +49,11 @@ function Header() {
               <a href="#experience">Work Experiences</a>
             </li>
           )}
+          {viewOpenSource && (
+            <li>
+              <a href="#opensource">Open Source</a>
+            </li>
+          )}
           {viewAchievement && (
             <li>
               <a href="#achievements">Achievements</a>
