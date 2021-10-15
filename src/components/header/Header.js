654e22b836d8db8e7a86e601ada951274f6461e2
Jesna k elizabeth jolly <92103227+Jesna-Dev@users.noreply.github.com>
2021-10-14 17:16:07 -0700

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -16,6 +16,7 @@ import {
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
+  const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
@@ -46,6 +47,11 @@ function Header() {
           {viewExperience && (
             <li>
               <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          {viewOpenSource && (
+            <li>
+              <a href="#opensource">Open Source</a>
             </li>
           )}
           {viewAchievement && (
